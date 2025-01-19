7a8645f1cb230b618ac4f2fb0e254c337b6ed4f5
satoshi1979 <satoshi19791212@gmail.com>
2025-01-19 17:25:03 +0900

    削除機能作成

--- app/Http/Controllers/MemoController.php
+++ app/Http/Controllers/MemoController.php
@@ -54,4 +54,12 @@ class MemoController extends Controller
 
         return redirect(route('memos.index'));
     }
+
+    public function destroy($id)
+    {
+        $memo = Memo::find($id);
+        $memo->delete();
+
+        return redirect(route('memos.index'));
+    }
 }
