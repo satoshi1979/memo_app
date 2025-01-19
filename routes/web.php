f478e18b877da2317698ecd3916525f902e6f61c
satoshi1979 <satoshi19791212@gmail.com>
2025-01-19 11:29:00 +0900

    プロジェクトの準備完了

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\MemoController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::resource('memos', MemoController::class);
